4ab2ec55c78f8dc388cba74626f6c192725e1400
ethanjdelano <ethanjdelano@gmail.com>
2014-04-02 10:49:21 +0100

    second version fizzbuzz complete

--- jquery.fizz-buzz.js
+++ jquery.fizz-buzz.js
@@ -1,15 +1,31 @@
 // JavaScript Document
 $(document).ready(function() {
 
-var number = 1
+var num = prompt("Please enter a number between 1 and 100")
 
+while (num % 1 != 0){
 
-while (number <= 100) {
+num = prompt("That is not a whole number, please enter a whole number value (ex: 1, 2, 3)")
+}
+
+while (num % 1 === 0){
+if(num > 100){
+num = prompt("That number is too high, please enter a new number")
+}
+if(num < 1){
+num = prompt("That number is too low, please enter a new number")	
+}
+break
+}
+
+var fizzbuzz = function(val) {
+	var number = 1
+	while (number <= val) {
 	if (number % 3 == 0){
 		
 		$(".container").append("fizz");
 		if (number % 5 == 0){
-			$(".container").append(" buzz");
+			$(".container").append("buzz");
 		}
 		$(".container").append("<br />");
 		number++
@@ -23,4 +39,10 @@ while (number <= 100) {
 	number++
 	}
 	}
+
+
+}
+
+fizzbuzz(num)
+
 })
